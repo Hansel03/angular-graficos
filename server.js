55066900db31309e7d43b736cf393019a8cbaab6
Hansel03 <hanselcapillo@hotmail.com>
2019-05-08 15:46:10 -0400

    Update server.js
    Se corrije error de sintaxis

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/dist'));
 
-app.length('/*', (req, res) => res.sendFile(path.join(__dirname)));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
 
 const server = http.createServer(app);
 
